6fadc8702f666e6cf0d3a45e4a2aefd98cf4c2d4
rra <rra@emachines.local>
2022-10-12 10:55:22 +0300

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import BitcoinPrice from './BitcoinPrice/BitcoinPrice';
+import EthereumPrice from './EthereumPrice/EthereumPrice';
+import LiteCoinPrice from './LiteCoinPrice/LiteCoinPrice';
+import Box from '@mui/material/Box';
 
 function App() {
   return (
-    <div className="App">
-        <BitcoinPrice/>
-    </div>
+    <Box sx={{ m: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Box sx={{ mr: 1 }}>
+          <BitcoinPrice/>
+        </Box>
+        <Box sx={{ ml: 1 }}>
+          <LiteCoinPrice/>
+        </Box>
+        <Box sx={{ ml: 1 }}>
+          <EthereumPrice/>
+        </Box>
+    </Box>
   );
 }
 
